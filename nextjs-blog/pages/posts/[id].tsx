81f2d37626f68632de191b54f65d504627eee0c9
Vitor Hugo Nunes Da Silva <vitorzcomputacao@gmail.com>
2021-04-15 16:58:25 -0300

    adding typescript

--- nextjs-blog/pages/posts/[id].tsx
+++ nextjs-blog/pages/posts/[id].tsx
@@ -2,8 +2,18 @@ import Layout from "../../components/layout";
 import Head from "next/head";
 import Date from "../../components/date";
 import utilStyles from "../../styles/utils.module.css";
+import { GetStaticProps, GetStaticPaths } from "next";
+import { getAllPostIds, getPostData } from "../../lib/posts";
 
-export default function Post({ postData }) {
+export default function Post({
+  postData,
+}: {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+}) {
   return (
     <Layout>
       <Head>
@@ -20,21 +30,19 @@ export default function Post({ postData }) {
   );
 }
 
-import { getAllPostIds, getPostData } from "../../lib/posts";
-
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id);
-  return {
-    props: {
-      postData,
-    },
-  };
-}
-
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds();
   return {
     paths,
     fallback: false,
   };
-}
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id as string);
+  return {
+    props: {
+      postData,
+    },
+  };
+};
