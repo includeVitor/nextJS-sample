587a38c1376add63fd641066b047ce2fc3a005af
Vitor Hugo Nunes Da Silva <vitorzcomputacao@gmail.com>
2021-04-16 10:48:19 -0300

    using Typescript

--- nextjs-blog/pages/index.tsx
+++ nextjs-blog/pages/index.tsx
@@ -3,8 +3,18 @@ import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 import Date from "../components/date";
+import { getSortedPostsData } from "../lib/posts";
+import { GetStaticProps } from "next";
 
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData,
+}: {
+  allPostsData: {
+    date: string;
+    title: string;
+    id: string;
+  }[];
+}) {
   return (
     <Layout home>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
@@ -27,13 +37,11 @@ export default function Home({ allPostsData }) {
   );
 }
 
-import { getSortedPostsData } from "../lib/posts";
-
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData();
   return {
     props: {
       allPostsData,
     },
   };
-}
+};
